0a3306e24db8c20bf58fd6e08235d0ca32da8271
phuriphat20 <155616139+phuriphat20@users.noreply.github.com>
2024-02-27 01:23:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Product from './product';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
